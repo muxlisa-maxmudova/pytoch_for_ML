4dbff62180036fc1d54d12609789a25c3de572db
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 19:02:02 +0500

    creating tensors with random nums

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ t = torch.tensor(data)
 np_array = np.array([1,2,3])
 t_from_np = torch.from_numpy(np_array)
 
+# 3. Random or Zero tensors
+rand_t = torch.rand(3,4)
+ones_t = torch.ones(2,2)
+print(rand_t)
