75aa9504bba5fe86819d45f4be6481c867afa4e1
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 20:03:44 +0500

    determining the gradient of a training model

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import torch
-import numpy as np
-t = torch.arange(2)
-print(t)
+x = torch.tensor([2.0], requires_grad=True)
+y = x**2 + 5  # y = 2^2 + 5 = 9
+
+# Calculate the derivative (dy/dx)
+y.backward()
+
+# The gradient is stored in x.grad
+print(x.grad) # Output: 4.0 (Because d/dx of x^2 is 2x, and 2*2=4)
