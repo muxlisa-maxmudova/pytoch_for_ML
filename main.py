98c870e002fca49908f7251fed0fb7e2429b7e6a
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 19:05:28 +0500

    shape and size functions to determine the size of the tensor

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import torch
 import numpy as np
-
-# 1. From a list
-data = [[1,2], [3,4]]
-t = torch.tensor(data)
-
-# 2. From NumPy
-np_array = np.array([1,2,3])
-t_from_np = torch.from_numpy(np_array)
-
-# 3. Random or Zero tensors
-rand_t = torch.rand(3,4)
-ones_t = torch.ones(2,2)
-print(rand_t)
+t = torch.rand(2,3)
+print(t.shape) # "shape" and "size" are the same things
+print(t.size())
